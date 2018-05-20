01efa16e4c56e6812d781520900dc6bad289b9c8
Eduard Zaydel <zaydel.eduard@gmail.com>
2018-05-20 15:29:00 +0300

    Added leasing screen UI

---
+++ app/src/main/java/com/wavesplatform/wallet/v2/ui/home/wallet/leasing/adapter/AdapterActiveLeasing.kt
@@ -0,0 +1,15 @@
+package com.wavesplatform.wallet.v2.ui.home.wallet.leasing.adapter
+
+import com.chad.library.adapter.base.BaseQuickAdapter
+import com.chad.library.adapter.base.BaseViewHolder
+import com.wavesplatform.wallet.R
+import com.wavesplatform.wallet.v2.ui.home.wallet.assets.adapter.TestObject
+import javax.inject.Inject
+
+class AdapterActiveLeasing @Inject constructor() : BaseQuickAdapter<TestObject, BaseViewHolder>(R.layout.wallet_leasing_item, null) {
+
+    override fun convert(helper: BaseViewHolder, item: TestObject) {
+
+    }
+}
+
