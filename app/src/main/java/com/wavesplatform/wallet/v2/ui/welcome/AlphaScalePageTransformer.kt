9498fbdbd204aff7766d50ae3e33f914f2c11d02
Eduard Zaydel <zaydel.eduard@gmail.com>
2018-05-24 11:37:08 +0300

    Fix view pager bug with scale

--- app/src/main/java/com/wavesplatform/wallet/v2/ui/welcome/AlphaScalePageTransformer.kt
+++ app/src/main/java/com/wavesplatform/wallet/v2/ui/welcome/AlphaScalePageTransformer.kt
@@ -2,25 +2,22 @@ package com.wavesplatform.wallet.v2.ui.welcome
 
 import android.support.v4.view.ViewPager
 import android.view.View
-import com.wavesplatform.wallet.R
 import kotlinx.android.synthetic.main.item_welcome.view.*
-import pers.victor.ext.screenWidth
-import timber.log.Timber
 
 
 class AlphaScalePageTransformer : ViewPager.PageTransformer {
     private val MIN_SCALE = 0.32f
     private val MIN_ALPHA = 0.4f
+    var decimal = -1f
 
     override fun transformPage(view: View, position: Float) {
-        Timber.i("SCREEN: %s", screenWidth)
-        val scaleFactor = Math.max(MIN_SCALE, 1 - Math.abs(position - 1.32f))
-        val alphaFactor = Math.max(MIN_ALPHA, 1 - Math.abs(position - 1.32f))
+        if (decimal == -1f) decimal = position - position.toInt()
+
+        val scaleFactor = Math.max(MIN_SCALE, 1 - Math.abs(position - (1 + decimal)))
+        val alphaFactor = Math.max(MIN_ALPHA, 1 - Math.abs(position - (1 + decimal)))
 
         view.image_welcome_photo.scaleX = scaleFactor
         view.image_welcome_photo.scaleY = scaleFactor
         view.image_welcome_photo.alpha = alphaFactor
-
-
     }
 }
