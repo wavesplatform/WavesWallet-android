e68cbb098bb55dde15d846ffdb6d81526cc2db6c
Eduard Zaydel <zaydel.eduard@gmail.com>
2018-07-05 19:40:02 +0300

    Added confirm dialog to delete address

--- app/src/main/java/com/wavesplatform/wallet/v2/ui/home/profile/address_book/edit/EditAddressActivity.kt
+++ app/src/main/java/com/wavesplatform/wallet/v2/ui/home/profile/address_book/edit/EditAddressActivity.kt
@@ -21,6 +21,12 @@ import com.wavesplatform.wallet.v2.ui.home.profile.address_book.AddressTestObjec
 import kotlinx.android.synthetic.main.activity_edit_address.*
 import pers.victor.ext.addTextChangedListener
 import pers.victor.ext.click
+import android.content.DialogInterface
+import android.content.DialogInterface.BUTTON_NEUTRAL
+import android.graphics.Typeface
+import android.support.v4.content.res.ResourcesCompat
+import android.support.v7.app.AlertDialog
+import android.widget.TextView
 
 
 class EditAddressActivity : BaseActivity(), EditAddressView {
@@ -61,9 +67,9 @@ class EditAddressActivity : BaseActivity(), EditAddressView {
             on({ s, start, before, count ->
                 presenter.addressFieldValid = edit_address.text.isNotEmpty()
                 isFieldsValid()
-                if (presenter.addressFieldValid){
+                if (presenter.addressFieldValid) {
                     edit_address.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.ic_deladdress_24_error_400, 0)
-                }else{
+                } else {
                     edit_address.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.ic_qrcode_24_basic_500, 0)
                 }
             })
@@ -86,10 +92,22 @@ class EditAddressActivity : BaseActivity(), EditAddressView {
         }
 
         button_delete.click {
-            val newIntent = Intent()
-            newIntent.putExtra(AddressBookActivity.BUNDLE_POSITION, intent.getIntExtra(BUNDLE_POSITION, -1))
-            setResult(Constants.RESULT_OK_NO_RESULT, newIntent)
-            finish()
+            val alertDialog = AlertDialog.Builder(this).create()
+            alertDialog.setTitle(getString(R.string.edit_address_delete_alert_title))
+            alertDialog.setMessage(getString(R.string.edit_address_delete_alert_description))
+            alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, getString(R.string.edit_address_delete_alert_delete),
+                    DialogInterface.OnClickListener { dialog, which ->
+                        dialog.dismiss()
+                        val newIntent = Intent()
+                        newIntent.putExtra(AddressBookActivity.BUNDLE_POSITION, intent.getIntExtra(BUNDLE_POSITION, -1))
+                        setResult(Constants.RESULT_OK_NO_RESULT, newIntent)
+                        finish()
+                    })
+            alertDialog.setButton(AlertDialog.BUTTON_NEGATIVE, getString(R.string.edit_address_delete_alert_cancel),
+                    DialogInterface.OnClickListener { dialog, which -> dialog.dismiss() })
+            alertDialog.show()
+            val titleTextView =  alertDialog?.findViewById<TextView>(R.id.alertTitle);
+            titleTextView?.typeface = ResourcesCompat.getFont(this, R.font.roboto_bold)
         }
 
         configureWithType()
@@ -97,7 +115,7 @@ class EditAddressActivity : BaseActivity(), EditAddressView {
 
     private fun configureWithType() {
         val type = intent.getIntExtra(AddressBookActivity.BUNDLE_TYPE, -1)
-        if (type == AddressBookActivity.SCREEN_TYPE_NOT_EDITABLE){
+        if (type == AddressBookActivity.SCREEN_TYPE_NOT_EDITABLE) {
             edit_address.setBackgroundColor(Color.TRANSPARENT)
             edit_address.isFocusable = false
             edit_address.isClickable = false
