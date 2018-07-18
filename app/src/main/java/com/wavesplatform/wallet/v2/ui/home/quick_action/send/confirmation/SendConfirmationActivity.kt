d80dd7582717574a99350c0edf72999d3f01096c
Eduard Zaydel <zaydel.eduard@gmail.com>
2018-07-18 14:12:48 +0300

    Added progress/success UI to send screen

--- app/src/main/java/com/wavesplatform/wallet/v2/ui/home/quick_action/send/confirmation/SendConfirmationActivity.kt
+++ app/src/main/java/com/wavesplatform/wallet/v2/ui/home/quick_action/send/confirmation/SendConfirmationActivity.kt
@@ -2,6 +2,7 @@ package com.wavesplatform.wallet.v2.ui.home.quick_action.send.confirmation
 
 import android.os.Bundle
 import android.view.View
+import android.view.animation.AnimationUtils
 import javax.inject.Inject
 
 import com.arellomobile.mvp.presenter.InjectPresenter
@@ -11,6 +12,11 @@ import com.wavesplatform.wallet.v2.ui.base.view.BaseActivity;
 import com.arellomobile.mvp.presenter.ProvidePresenter;
 import com.wavesplatform.wallet.R
 import kotlinx.android.synthetic.main.activity_send_confirmation.*
+import pers.victor.ext.click
+import pers.victor.ext.gone
+import pers.victor.ext.invisiable
+import pers.victor.ext.visiable
+import pyxis.uzuki.live.richutilskt.utils.runDelayed
 
 
 class SendConfirmationActivity : BaseActivity(), SendConfirmationView {
@@ -24,6 +30,23 @@ class SendConfirmationActivity : BaseActivity(), SendConfirmationView {
 
     override fun onViewReady(savedInstanceState: Bundle?) {
         setupToolbar(toolbar_view, View.OnClickListener { onBackPressed() }, true, getString(R.string.send_confirmation_toolbar_title), R.drawable.ic_toolbar_back_white)
+
+        button_confirm.click {
+            toolbar_view.invisiable()
+            card_content.gone()
+            card_progress.visiable()
+            var rotation = AnimationUtils.loadAnimation(this@SendConfirmationActivity, R.anim.rotate);
+            rotation.fillAfter = true;
+            image_loader.startAnimation(rotation);
+            runDelayed(2000, {
+                image_loader.clearAnimation()
+                card_progress.gone()
+                relative_success.visiable()
+                button_okay.click {
+                    finish()
+                }
+            })
+        }
     }
 
 }
