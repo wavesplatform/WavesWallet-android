f80cac2f55d05a001d85ba034b37fbfdd29a252e
Ershov <aershov@wavesplatform.com>
2018-08-30 16:12:43 +0300

    Round icons

--- app/src/main/java/com/wavesplatform/wallet/v2/ui/auth/choose_account/ChooseAccountAdapter.kt
+++ app/src/main/java/com/wavesplatform/wallet/v2/ui/auth/choose_account/ChooseAccountAdapter.kt
@@ -2,14 +2,19 @@ package com.wavesplatform.wallet.v2.ui.auth.choose_account
 
 import android.graphics.Color
 import android.widget.ImageView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.chad.library.adapter.base.BaseQuickAdapter
 import com.chad.library.adapter.base.BaseViewHolder
 import com.daimajia.swipe.SwipeLayout
 import com.wavesplatform.wallet.R
+import com.wavesplatform.wallet.v1.util.AddressUtil
 import com.wavesplatform.wallet.v2.ui.custom.Identicon
 import com.wavesplatform.wallet.v2.ui.home.profile.address_book.AddressBookUser
+import org.apache.commons.io.Charsets
 import pers.victor.ext.click
 import pyxis.uzuki.live.richutilskt.utils.runDelayed
+import java.util.*
 import javax.inject.Inject
 
 class ChooseAccountAdapter @Inject constructor() : BaseQuickAdapter<AddressBookUser, BaseViewHolder>(R.layout.choose_address_layout, null) {
@@ -20,10 +25,19 @@ class ChooseAccountAdapter @Inject constructor() : BaseQuickAdapter<AddressBookU
     override fun convert(helper: BaseViewHolder, item: AddressBookUser) {
         helper.setText(R.id.text_address, item.address)
                 .setText(R.id.text_name, item.name)
-                .setImageBitmap(R.id.image_asset, Identicon.create(item.address,
-                        Identicon.Options.Builder()
-                                .setBlankColor(Color.WHITE)
-                                .create()))
+
+        val rnd = Random()
+        val color = Color.argb(255,
+                rnd.nextInt(256),
+                rnd.nextInt(256),
+                rnd.nextInt(256))
+
+        Glide.with(helper.itemView.context)
+                .load(Identicon.create(
+                        item.address,
+                        Identicon.Options.Builder().setBlankColor(color).create()))
+                .apply(RequestOptions().circleCrop())
+                .into(helper.getView(R.id.image_asset))
 
         val swipeLayout = helper.getView<SwipeLayout>(R.id.swipe_layout)
         swipeLayout.showMode = SwipeLayout.ShowMode.LayDown
