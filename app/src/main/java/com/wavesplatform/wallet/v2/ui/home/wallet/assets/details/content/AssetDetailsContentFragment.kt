e186b631615a27e7f6fe3da59b60411b58083d65
Eduard Zaydel <zaydel.eduard@gmail.com>
2018-09-06 20:20:31 +0300

    Added last asset transactions logic, asset info logic, some fixes

--- app/src/main/java/com/wavesplatform/wallet/v2/ui/home/wallet/assets/details/content/AssetDetailsContentFragment.kt
+++ app/src/main/java/com/wavesplatform/wallet/v2/ui/home/wallet/assets/details/content/AssetDetailsContentFragment.kt
@@ -12,9 +12,11 @@ import com.wavesplatform.wallet.v2.ui.home.wallet.assets.details.HistoryTransact
 import com.wavesplatform.wallet.v2.ui.home.wallet.assets.token_burn.TokenBurnActivity
 import com.wavesplatform.wallet.v2.util.copyToClipboard
 import com.wavesplatform.wallet.v2.util.launchActivity
+import com.wavesplatform.wallet.v2.util.notNull
 import kotlinx.android.synthetic.main.fragment_asset_details_content.*
 import pers.victor.ext.click
 import pers.victor.ext.dp2px
+import pyxis.uzuki.live.richutilskt.utils.runAsync
 import java.text.SimpleDateFormat
 import java.util.*
 import javax.inject.Inject
@@ -41,17 +43,13 @@ class AssetDetailsContentFragment : BaseFragment(), AssetDetailsContentView {
 
 
     override fun onViewReady(savedInstanceState: Bundle?) {
-        presenter.loadBundle()
-    }
+        presenter.assetBalance = arguments?.getParcelable<AssetBalance>(BUNDLE_ASSET)
 
-    override fun showData(data: ArrayList<HistoryItem>) {
-        historyAdapter.items = data
         view_pager_transaction_history.adapter = historyAdapter
         view_pager_transaction_history.offscreenPageLimit = 3
         view_pager_transaction_history.clipToPadding = false
         view_pager_transaction_history.setPadding(dp2px(14), 0, dp2px(14), 0);
         view_pager_transaction_history.pageMargin = dp2px(7)
-        historyAdapter.notifyDataSetChanged()
 
         image_copy_issuer.click {
             text_view_issuer_value.copyToClipboard(it)
@@ -61,22 +59,53 @@ class AssetDetailsContentFragment : BaseFragment(), AssetDetailsContentView {
             text_view_id_value.copyToClipboard(it)
         }
 
-        presenter.assetBalance = arguments?.getParcelable<AssetBalance>(BUNDLE_ASSET)
+        card_burn.click {
+            launchActivity<TokenBurnActivity> { }
+        }
 
         fillInformation(presenter.assetBalance)
 
-        card_burn.click {
-            launchActivity<TokenBurnActivity> {  }
-        }
+
+        runAsync({
+            presenter.loadLastTransactions()
+        })
+    }
+
+    override fun showLastTransactions(data: MutableList<HistoryItem>) {
+        historyAdapter.items = data
+        historyAdapter.notifyDataSetChanged()
     }
 
     private fun fillInformation(assetBalance: AssetBalance?) {
         text_view_asset_name_value.text = assetBalance?.getName()
-        text_view_issuer_value.text = assetBalance?.issueTransaction?.sender
-        text_view_id_value.text = assetBalance?.issueTransaction?.assetId
-        text_reusable_value.text = if (assetBalance?.reissuable!!) getString(R.string.asset_details_reissuable) else getString(R.string.asset_details_not_reissuable)
-        text_issue_date_value.text = formatter.format(Date(assetBalance.issueTransaction?.timestamp!!))
-        text_description.text = assetBalance.issueTransaction?.description
+        text_reusable_value.text =
+                if (assetBalance?.reissuable == true) getString(R.string.asset_details_reissuable)
+                else getString(R.string.asset_details_not_reissuable)
+        text_description.text =
+                if (assetBalance?.issueTransaction?.description.isNullOrEmpty()) getString(R.string.common_dash)
+                else assetBalance?.issueTransaction?.description
+        text_view_issuer_value.text =
+                if (assetBalance?.issueTransaction?.sender.isNullOrEmpty()) getString(R.string.common_dash)
+                else assetBalance?.issueTransaction?.sender
+        text_view_id_value.text =
+                if (assetBalance?.issueTransaction?.assetId.isNullOrEmpty()) getString(R.string.common_dash)
+                else assetBalance?.issueTransaction?.assetId
+        text_issue_date_value.text = getString(R.string.common_dash)
+        assetBalance?.issueTransaction?.timestamp.notNull {
+            text_issue_date_value.text = formatter.format(Date(it))
+        }
+
+        if (assetBalance?.isWaves() == true) {
+
+        } else if (assetBalance?.isGateway == true) {
+
+        } else if (assetBalance?.isFlatMoney == true) {
+
+        } else if (assetBalance?.isSpam == true) {
+
+        } else {
+
+        }
     }
 
 }
--- app/src/main/java/com/wavesplatform/wallet/v2/ui/home/wallet/assets/details/content/AssetDetailsContentView.kt
+++ app/src/main/java/com/wavesplatform/wallet/v2/ui/home/wallet/assets/details/content/AssetDetailsContentView.kt
@@ -2,8 +2,7 @@ package com.wavesplatform.wallet.v2.ui.home.wallet.assets.details.content
 
 import com.wavesplatform.wallet.v2.ui.base.view.BaseMvpView
 import com.wavesplatform.wallet.v2.ui.home.history.HistoryItem
-import java.util.*
 
 interface AssetDetailsContentView : BaseMvpView {
-    fun showData(data: ArrayList<HistoryItem>)
+    fun showLastTransactions(data: MutableList<HistoryItem>)
 }
