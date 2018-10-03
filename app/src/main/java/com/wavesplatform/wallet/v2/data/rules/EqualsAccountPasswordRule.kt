4f6bae362eab8f0cdad1d6835a7b88ad2eeb8173
Eduard Zaydel <zaydel.eduard@gmail.com>
2018-10-03 15:34:13 +0300

    Added validation to change password screen, ask pass code before enter to screen

---
+++ app/src/main/java/com/wavesplatform/wallet/v2/data/rules/EqualsAccountPasswordRule.kt
@@ -0,0 +1,37 @@
+package com.wavesplatform.wallet.v2.data.rules
+
+import android.support.annotation.StringRes
+import com.wavesplatform.wallet.App
+import com.wavesplatform.wallet.R.id.edit_old_password
+import com.wavesplatform.wallet.v1.data.auth.WavesWallet
+
+import io.github.anderscheow.validator.rules.BaseRule
+
+class EqualsAccountPasswordRule : BaseRule {
+
+    constructor() : super("Value must not be empty") {}
+
+    constructor(@StringRes errorRes: Int) : super(errorRes) {}
+
+    constructor(errorMessage: String) : super(errorMessage) {}
+
+    override fun validate(value: Any?): Boolean {
+        if (value == null) {
+            throw NullPointerException()
+        }
+
+        if (value is String) {
+            return try {
+                val oldWallet = WavesWallet(
+                        App.getAccessManager().getCurrentWavesWalletEncryptedData(),
+                        value
+                )
+                true
+            } catch (e: Exception) {
+                false
+            }
+        }
+
+        throw ClassCastException("Required String value")
+    }
+}
