43fb828e7ad9bad74f356c4e14bdf509141fe659
Eduard Zaydel <zaydel.eduard@gmail.com>
2018-10-08 18:43:18 +0300

    Rewrite get all active leasing transcation logic, fix inccorect transactions in history screen

--- app/src/main/java/com/wavesplatform/wallet/v2/ui/home/wallet/leasing/LeasingPresenter.kt
+++ app/src/main/java/com/wavesplatform/wallet/v2/ui/home/wallet/leasing/LeasingPresenter.kt
@@ -1,7 +1,13 @@
 package com.wavesplatform.wallet.v2.ui.home.wallet.leasing
 
+import android.util.Log
 import com.arellomobile.mvp.InjectViewState
+import com.vicpin.krealmextensions.queryAsSingle
+import com.vicpin.krealmextensions.queryAsync
+import com.wavesplatform.wallet.R.layout.test
 import com.wavesplatform.wallet.v1.data.rxjava.RxUtil
+import com.wavesplatform.wallet.v2.data.Constants
+import com.wavesplatform.wallet.v2.data.model.local.LeasingStatus
 import com.wavesplatform.wallet.v2.data.model.remote.response.AssetBalance
 import com.wavesplatform.wallet.v2.data.model.remote.response.Transaction
 import com.wavesplatform.wallet.v2.ui.base.presenter.BasePresenter
@@ -16,18 +22,23 @@ class LeasingPresenter @Inject constructor() : BasePresenter<LeasingView>() {
 
     fun getActiveLeasing() {
         runAsync {
-            addSubscription(Observable.zip(nodeDataManager.loadWavesBalance(), nodeDataManager.activeLeasing(),
-                    BiFunction { t1: AssetBalance, t2: List<Transaction> ->
-                        return@BiFunction Pair(t1, t2)
-                    })
-                    .compose(RxUtil.applySchedulersToObservable())
-                    .subscribe {
-                        val leasedSum = it.second.sumByLong { it.amount }
-                        viewState.showBalances(it.first,
-                                leasedSum, it.first.balance?.minus(leasedSum))
-                        viewState.showActiveLeasingTransaction(it.second)
-                    })
+                addSubscription(Observable.zip(nodeDataManager.loadWavesBalance(),
+                        queryAsSingle<Transaction> {
+                            equalTo("status", LeasingStatus.ACTIVE.status)
+                                    .and()
+                                    .equalTo("transactionTypeId", Constants.ID_STARTED_LEASING_TYPE)
+                        }.toObservable(),
+                        BiFunction { t1: AssetBalance, t2: List<Transaction> ->
+                            return@BiFunction Pair(t1, t2)
+                        })
+                        .compose(RxUtil.applySchedulersToObservable())
+                        .subscribe {
+                            val leasedSum = it.second.sumByLong { it.amount }
+                            viewState.showBalances(it.first,
+                                    leasedSum, it.first.balance?.minus(leasedSum))
+                            viewState.showActiveLeasingTransaction(it.second)
+                        })
+            }
         }
-    }
 
-}
+    }
