e39bcfafd7f8e591e9b2fa4e3ea66d324147781c
Eduard Zaydel <zaydel.eduard@gmail.com>
2018-10-12 14:39:12 +0300

    Added seed check when delete account in choose account screen

--- app/src/main/java/com/wavesplatform/wallet/v2/ui/auth/choose_account/ChooseAccountActivity.kt
+++ app/src/main/java/com/wavesplatform/wallet/v2/ui/auth/choose_account/ChooseAccountActivity.kt
@@ -5,11 +5,13 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AlertDialog
 import android.support.v7.widget.LinearLayoutManager
+import android.view.LayoutInflater
 import android.view.View
 import com.arellomobile.mvp.presenter.InjectPresenter
 import com.arellomobile.mvp.presenter.ProvidePresenter
 import com.wavesplatform.wallet.App
 import com.wavesplatform.wallet.R
+import com.wavesplatform.wallet.v1.util.PrefsUtil
 import com.wavesplatform.wallet.v2.data.Constants
 import com.wavesplatform.wallet.v2.ui.auth.choose_account.edit.EditAccountNameActivity
 import com.wavesplatform.wallet.v2.ui.auth.passcode.enter.EnterPassCodeActivity
@@ -41,7 +43,7 @@ class ChooseAccountActivity : BaseActivity(), ChooseAccountView, ChooseAccountOn
 
     override fun onViewReady(savedInstanceState: Bundle?) {
         setStatusBarColor(R.color.basic50)
-        setupToolbar(toolbar_view,  true,
+        setupToolbar(toolbar_view, true,
                 getString(R.string.choose_account), R.drawable.ic_toolbar_back_black)
 
         recycle_addresses.layoutManager = LinearLayoutManager(this)
@@ -79,19 +81,23 @@ class ChooseAccountActivity : BaseActivity(), ChooseAccountView, ChooseAccountOn
     }
 
     override fun onDeleteClicked(position: Int) {
+        val item = adapter.getItem(position) as AddressBookUser
+        val guid = App.getAccessManager().findGuidBy(item.address)
+
         val alertDialog = AlertDialog.Builder(this).create()
         alertDialog.setTitle(getString(R.string.choose_account_delete_title))
         alertDialog.setMessage(getString(R.string.choose_account_delete_msg))
+        if (prefsUtil.getGuidValue(guid, PrefsUtil.KEY_SKIP_BACKUP, true)) {
+            alertDialog.setView(inflate(R.layout.delete_account_warning_layout, null))
+        }
         alertDialog.setButton(AlertDialog.BUTTON_POSITIVE,
                 getString(R.string.choose_account_yes)) { dialog, which ->
             dialog.dismiss()
-            val item = adapter.getItem(position)
-            if (item is AddressBookUser) {
-                App.getAccessManager().deleteWavesWallet(item.address)
-                adapter.remove(position)
-            }
+
+            App.getAccessManager().deleteWavesWallet(item.address)
+            adapter.remove(position)
+
             showSuccess(R.string.choose_account_deleted, R.id.content)
-            adapter.notifyDataSetChanged()
         }
         alertDialog.setButton(AlertDialog.BUTTON_NEGATIVE,
                 getString(R.string.choose_account_cancel)) { dialog, which ->
