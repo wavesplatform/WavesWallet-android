68f2e494980e42cf0f3e96e5094cb68f142fa3ac
Ershov <aershov@wavesplatform.com>
2018-10-19 17:25:47 +0300

    Fix. Skeletons with rounded conors

--- app/src/main/java/com/wavesplatform/wallet/v2/data/Constants.kt
+++ app/src/main/java/com/wavesplatform/wallet/v2/data/Constants.kt
@@ -108,11 +108,11 @@ object Constants {
             Pair("474jTeYx2r2Va35794tCScAXWJG9hU2HcgxzMowaZUnu", R.drawable.logo_ethereum_48),
             Pair("Ft8X1v1LTa1ABafufpaCWyVj8KkaxUWE6xBhW6sNFJck", R.drawable.logo_usd_48),
             Pair("Gtb1WRznfchDnTh37ezoDTJ4wcoKaRsKqKjJjy7nm2zU", R.drawable.logo_euro_48),
+            Pair("2mX5DzVKWrAJw8iwdJnV2qtoeVG9h5nTDpTqC1wb1WEN", R.drawable.logo_lira_48),
             Pair("HZk1mbfuJpmxU1Fs4AX5MWLVYtctsNcg6e2C6VKqK8zk", R.drawable.logo_ltc_48),
             Pair("5WvPKSJXzVE2orvbkJ8wsQmmQKqTv9sGBPksV4adViw3", R.drawable.logo_monero_48),
             Pair("zMFqXuoyrn5w17PFurTqxB7GsS71fp9dfk6XFwxbPCy", R.drawable.logo_bitcoincash_48),
             Pair("BrjUWjndUanm5VsJkbUip8VRYy6LWJePtxya3FNv4TQa", R.drawable.logo_zec_48),
-            Pair("2mX5DzVKWrAJw8iwdJnV2qtoeVG9h5nTDpTqC1wb1WEN", R.drawable.logo_lira_48),
             Pair("B3uGHFRpSUuGEDWjqB9LWWxafQj8VTvpMucEyoxzws5H", R.drawable.logo_dash_48))
 //    val URL_ROADMAP = "https://wavesplatform.com/files/docs/Waves_terms_and_conditions.pdf"
 
@@ -122,11 +122,12 @@ object Constants {
             "HZk1mbfuJpmxU1Fs4AX5MWLVYtctsNcg6e2C6VKqK8zk",
             "5WvPKSJXzVE2orvbkJ8wsQmmQKqTv9sGBPksV4adViw3",
             "zMFqXuoyrn5w17PFurTqxB7GsS71fp9dfk6XFwxbPCy",
-            "2mX5DzVKWrAJw8iwdJnV2qtoeVG9h5nTDpTqC1wb1WEN",
+            "BrjUWjndUanm5VsJkbUip8VRYy6LWJePtxya3FNv4TQa",
             "B3uGHFRpSUuGEDWjqB9LWWxafQj8VTvpMucEyoxzws5H")
 
     val defaultFiat = arrayOf(
             "Gtb1WRznfchDnTh37ezoDTJ4wcoKaRsKqKjJjy7nm2zU",
-            "Ft8X1v1LTa1ABafufpaCWyVj8KkaxUWE6xBhW6sNFJck")
+            "Ft8X1v1LTa1ABafufpaCWyVj8KkaxUWE6xBhW6sNFJck",
+            "2mX5DzVKWrAJw8iwdJnV2qtoeVG9h5nTDpTqC1wb1WEN")
 
 }
