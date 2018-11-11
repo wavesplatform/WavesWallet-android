e7c255193eb96827fd4cb4b68e7729758ae54d86
Ershov <aershov@wavesplatform.com>
2018-11-12 00:59:35 +0300

    Proguard fix

--- app/src/main/java/com/wavesplatform/wallet/v2/data/remote/MatcherService.kt
+++ app/src/main/java/com/wavesplatform/wallet/v2/data/remote/MatcherService.kt
@@ -1,19 +1,9 @@
 package com.wavesplatform.wallet.v2.data.remote
 
-import com.wavesplatform.wallet.v1.payload.TransactionsInfo
-import com.wavesplatform.wallet.v1.payload.WavesBalance
-import com.wavesplatform.wallet.v1.request.IssueTransactionRequest
-import com.wavesplatform.wallet.v1.request.ReissueTransactionRequest
-import com.wavesplatform.wallet.v1.request.TransferTransactionRequest
-import com.wavesplatform.wallet.v2.data.model.remote.request.AliasRequest
-import com.wavesplatform.wallet.v2.data.model.remote.request.CancelLeasingRequest
-import com.wavesplatform.wallet.v2.data.model.remote.request.CreateLeasingRequest
-import com.wavesplatform.wallet.v2.data.model.remote.response.Alias
-import com.wavesplatform.wallet.v2.data.model.remote.response.AssetBalances
-import com.wavesplatform.wallet.v2.data.model.remote.response.Height
-import com.wavesplatform.wallet.v2.data.model.remote.response.Transaction
 import io.reactivex.Observable
-import retrofit2.http.*
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Path
 
 interface MatcherService {
 
