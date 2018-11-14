b8cae7bca5dccd038d494602abc4f7109de83962
Eduard Zaydel <zaydel.eduard@gmail.com>
2018-11-14 19:01:16 +0200

    Fix. Alias. Added balance validation, added API error handler

--- app/src/main/java/com/wavesplatform/wallet/v2/ui/home/profile/addresses/alias/create/CreateAliasPresenter.kt
+++ app/src/main/java/com/wavesplatform/wallet/v2/ui/home/profile/addresses/alias/create/CreateAliasPresenter.kt
@@ -1,7 +1,11 @@
 package com.wavesplatform.wallet.v2.ui.home.profile.addresses.alias.create
 
 import com.arellomobile.mvp.InjectViewState
+import com.wavesplatform.wallet.v2.data.exception.RetrofitException
 import com.wavesplatform.wallet.v2.data.model.remote.request.AliasRequest
+import com.wavesplatform.wallet.v2.data.model.remote.response.AssetBalance
+import com.wavesplatform.wallet.v2.data.model.remote.response.AssetBalances
+import com.wavesplatform.wallet.v2.data.model.remote.response.ErrorResponse
 import com.wavesplatform.wallet.v2.ui.base.presenter.BasePresenter
 import com.wavesplatform.wallet.v2.util.RxUtil
 import javax.inject.Inject
@@ -10,6 +14,7 @@ import javax.inject.Inject
 class CreateAliasPresenter @Inject constructor() : BasePresenter<CreateAliasView>() {
 
     var aliasRequest: AliasRequest = AliasRequest()
+    var wavesBalance: AssetBalance = AssetBalance()
 
     fun loadAlias(alias: String) {
         addSubscription(apiDataManager.loadAlias(alias)
@@ -21,13 +26,33 @@ class CreateAliasPresenter @Inject constructor() : BasePresenter<CreateAliasView
                 }))
     }
 
+    fun loadWavesBalance() {
+        addSubscription(nodeDataManager.loadWavesBalance()
+                .compose(RxUtil.applyObservableDefaultSchedulers())
+                .subscribe({
+                    wavesBalance = it
+                }, {
+                    it.printStackTrace()
+                }))
+    }
+
     fun createAlias(alias: String?) {
         aliasRequest.alias = alias
 
+        viewState.showProgressBar(true)
+
         addSubscription(nodeDataManager.createAlias(aliasRequest)
                 .compose(RxUtil.applyObservableDefaultSchedulers())
-                .subscribe {
+                .subscribe({
+                    viewState.showProgressBar(false)
                     viewState.successCreateAlias(it)
-                })
+                }, {
+                    it.printStackTrace()
+                    viewState.showProgressBar(false)
+                    if (it is RetrofitException) {
+                        val response = it.getErrorBodyAs(ErrorResponse::class.java)
+                        viewState.failedCreateAlias(response?.message)
+                    }
+                }))
     }
 }
