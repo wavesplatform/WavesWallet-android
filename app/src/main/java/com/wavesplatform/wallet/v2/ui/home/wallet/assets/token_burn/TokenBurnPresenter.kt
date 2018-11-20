2c07215809fff8c5f8ea178f88b055068f9ab79d
Eduard Zaydel <zaydel.eduard@gmail.com>
2018-11-20 14:51:05 +0200

    Fix. DROID-297. Added validation to token burn screen, rewrite logic

--- app/src/main/java/com/wavesplatform/wallet/v2/ui/home/wallet/assets/token_burn/TokenBurnPresenter.kt
+++ app/src/main/java/com/wavesplatform/wallet/v2/ui/home/wallet/assets/token_burn/TokenBurnPresenter.kt
@@ -1,10 +1,27 @@
 package com.wavesplatform.wallet.v2.ui.home.wallet.assets.token_burn
 
 import com.arellomobile.mvp.InjectViewState
+import com.wavesplatform.wallet.v2.data.model.remote.response.AssetBalance
 import com.wavesplatform.wallet.v2.ui.base.presenter.BasePresenter
+import com.wavesplatform.wallet.v2.util.RxUtil
+import org.fingerlinks.mobile.android.navigator.builder.Builders
 import javax.inject.Inject
 
 @InjectViewState
 class TokenBurnPresenter @Inject constructor() : BasePresenter<TokenBurnView>() {
+    var quantityValidation = false
+    var wavesBalance: AssetBalance = AssetBalance()
+    var assetBalance = AssetBalance()
 
+    fun isAllFieldsValid(): Boolean {
+        return quantityValidation
+    }
+
+    fun loadWavesBalance() {
+        addSubscription(nodeDataManager.loadWavesBalance()
+                .compose(RxUtil.applyObservableDefaultSchedulers())
+                .subscribe {
+                    wavesBalance = it
+                })
+    }
 }
