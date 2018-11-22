3eb6bfc3fe564b9de8f569d436575b4c3414be08
Ershov <aershov@wavesplatform.com>
2018-11-22 16:30:45 +0300

    Merge

--- app/src/main/java/com/wavesplatform/wallet/v2/ui/home/wallet/assets/details/content/AssetDetailsContentPresenter.kt
+++ app/src/main/java/com/wavesplatform/wallet/v2/ui/home/wallet/assets/details/content/AssetDetailsContentPresenter.kt
@@ -2,17 +2,14 @@ package com.wavesplatform.wallet.v2.ui.home.wallet.assets.details.content
 
 import com.arellomobile.mvp.InjectViewState
 import com.vicpin.krealmextensions.queryAllAsSingle
-import com.vicpin.krealmextensions.queryAllAsync
 import com.wavesplatform.wallet.v2.data.model.local.HistoryItem
 import com.wavesplatform.wallet.v2.data.model.remote.response.AssetBalance
 import com.wavesplatform.wallet.v2.data.model.remote.response.Transaction
 import com.wavesplatform.wallet.v2.data.model.remote.response.TransactionType
 import com.wavesplatform.wallet.v2.ui.base.presenter.BasePresenter
-import com.wavesplatform.wallet.v2.data.model.local.HistoryItem
 import com.wavesplatform.wallet.v2.util.RxUtil
 import com.wavesplatform.wallet.v2.util.isWaves
 import com.wavesplatform.wallet.v2.util.transactionType
-import io.reactivex.Observable
 import pyxis.uzuki.live.richutilskt.utils.runAsync
 import pyxis.uzuki.live.richutilskt.utils.runOnUiThread
 import javax.inject.Inject
@@ -22,21 +19,23 @@ class AssetDetailsContentPresenter @Inject constructor() : BasePresenter<AssetDe
 
     var assetBalance: AssetBalance? = null
 
-    fun loadLastTransactions(assetId: String) {
+    fun loadLastTransactionsFor(assetId: String) {
         runAsync {
             addSubscription(queryAllAsSingle<Transaction>().toObservable()
                     .map {
                         return@map it
                                 .sortedByDescending { it.timestamp }
-                                .filter { it.transactionType() != TransactionType.MASS_SPAM_RECEIVE_TYPE || it.transactionType() != TransactionType.SPAM_RECEIVE_TYPE }
-                                .filter {
-                                    if (assetId.isWaves()) {
-                                        return@filter it.assetId.isNullOrEmpty()
-                                    } else if (it.order1?.assetPair?.amountAssetObject?.id == assetId
-                                            || it.order1?.assetPair?.priceAssetObject?.id == assetId) {
-                                        return@filter true
-                                    } else {
-                                        return@filter it.assetId == assetId
+                                .filter { transaction ->
+                                    return@filter isSpam(transaction)
+                                }
+                                .filter { transaction ->
+                                    when {
+                                        assetId.isWaves() ->
+                                            return@filter transaction.assetId.isNullOrEmpty()
+                                        isAssetIdInExchange(transaction, assetId) ->
+                                            return@filter true
+                                        else ->
+                                            return@filter transaction.assetId == assetId
                                     }
                                 }
                                 .mapTo(ArrayList()) { HistoryItem(HistoryItem.TYPE_DATA, it) }
@@ -44,13 +43,25 @@ class AssetDetailsContentPresenter @Inject constructor() : BasePresenter<AssetDe
                                 .toMutableList()
                     }
                     .compose(RxUtil.applyObservableDefaultSchedulers())
-                    .subscribe({
+                    .subscribe({ list ->
                         runOnUiThread {
                             viewState.showLastTransactions(list)
                         }
                     }, {
                         it.printStackTrace()
+                        runOnUiThread {
+                            viewState.showLastTransactions(
+                                    emptyList<HistoryItem>().toMutableList())
+                        }
                     }))
         }
     }
+
+    private fun isAssetIdInExchange(transaction: Transaction, assetId: String) =
+            (transaction.order1?.assetPair?.amountAssetObject?.id == assetId
+                    || transaction.order1?.assetPair?.priceAssetObject?.id == assetId)
+
+    private fun isSpam(transaction: Transaction) =
+            transaction.transactionType() != TransactionType.MASS_SPAM_RECEIVE_TYPE
+                    || transaction.transactionType() != TransactionType.SPAM_RECEIVE_TYPE
 }
