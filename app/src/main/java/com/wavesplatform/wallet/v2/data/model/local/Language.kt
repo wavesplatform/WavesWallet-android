fcc55a7ee5dc29f05b3b6ef25977deea1dc13059
Ershov <aershov@wavesplatform.com>
2018-11-27 12:20:31 +0300

    Lang prepare

--- app/src/main/java/com/wavesplatform/wallet/v2/data/model/local/Language.kt
+++ app/src/main/java/com/wavesplatform/wallet/v2/data/model/local/Language.kt
@@ -16,13 +16,20 @@ enum class Language(@DrawableRes var image: Int,
                     var code: String) {
     ENGLISH(R.drawable.ic_flag_18_britain, R.string.choose_language_english, "en"),
     RUSSIAN(R.drawable.ic_flag_18_rus, R.string.choose_language_russia, "ru"),
-    SPANISH(R.drawable.ic_flag_18_spain, R.string.choose_language_spain, "es"),
-    // TURKISH(R.drawable.ic_flag_18_turkey, R.string.choose_language_turkey, "tr"),
     KOREAN(R.drawable.ic_flag_18_korea, R.string.choose_language_korea, "ko"),
-    // CHINESE(R.drawable.ic_flag_18_china, "zh),
-    // HINDI(R.drawable.ic_flag_18_hindi, "hi),
-    // DANISH(R.drawable.ic_flag_18_danish, "dn),
-    DUTCH(R.drawable.ic_flag_18_nederland, R.string.choose_language_nederlands, "nl");
+    CHINESE(R.drawable.ic_flag_18_china, R.string.choose_language_china, "zh"),
+    // TURKISH(R.drawable.ic_flag_18_turkey, R.string.choose_language_turkey, "tr"),
+    DUTCH(R.drawable.ic_flag_18_nederland, R.string.choose_language_nederlands, "nl"),
+    HINDI(R.drawable.ic_flag_18_hindi, R.string.choose_language_hindi,"hi"),
+    SPANISH(R.drawable.ic_flag_18_spain, R.string.choose_language_spain, "es"),
+    // DANISH(R.drawable.ic_flag_18_danish, R.string.choose_language_danish, "dn"),
+    INDONESIAN(R.drawable.ic_flag_18_indonesia, R.string.choose_language_indonesian, "id"),
+    GERMAN(R.drawable.ic_flag_18_germany, R.string.choose_language_german, "de"),
+    // JAPAN(R.drawable.ic_flag_18_japan, R.string.choose_language_japan, "jp"),
+    PORTUGUESE(R.drawable.ic_flag_18_portugal, R.string.choose_language_portuguese, "pt"),
+    BRAZILIAN(R.drawable.ic_flag_18_brazil, R.string.choose_language_brazilian, "pt-rBR"),
+    // POLISH(R.drawable.ic_flag_18_polszczyzna, R.string.choose_language_polish, "pl")
+    ;
 
     companion object {
 
