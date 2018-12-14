2518dd5dc1699626ea73e4a0d012186e577b4ff6
Eduard Zaydel <zaydel.eduard@gmail.com>
2018-12-14 16:17:59 +0200

    Fix. DROID-316. Rewrite UI/Logic of asset sorting screen

---
+++ app/src/main/java/com/wavesplatform/wallet/v2/util/drag_helper/ItemDragListener.kt
@@ -0,0 +1,12 @@
+package com.wavesplatform.wallet.v2.util.drag_helper
+
+import android.support.v7.widget.RecyclerView
+import android.view.View
+
+interface ItemDragListener {
+    fun onStartDrag(viewHolder: RecyclerView.ViewHolder, position: Int)
+
+    fun onMoved(fromHolder: View?, fromPosition: Int, toHolder: View?, toPosition: Int)
+
+    fun onEndDrag(viewHolder: RecyclerView.ViewHolder, position: Int)
+}
