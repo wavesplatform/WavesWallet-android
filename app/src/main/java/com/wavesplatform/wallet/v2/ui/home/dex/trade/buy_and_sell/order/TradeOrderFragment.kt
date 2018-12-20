77022605a497cff603309dcfee07ac7fc9da916d
Eduard Zaydel <zaydel.eduard@gmail.com>
2018-12-20 14:42:33 +0200

    Fix. Minus dex fee from current value

--- app/src/main/java/com/wavesplatform/wallet/v2/ui/home/dex/trade/buy_and_sell/order/TradeOrderFragment.kt
+++ app/src/main/java/com/wavesplatform/wallet/v2/ui/home/dex/trade/buy_and_sell/order/TradeOrderFragment.kt
@@ -147,8 +147,8 @@ class TradeOrderFragment : BaseFragment(), TradeOrderView {
                 }
                 .map {
                     if (presenter.orderType == TradeBuyAndSellBottomSheetFragment.SELL_TYPE) {
-                        val isValid = it.toBigDecimal() <= MoneyUtil.getScaledText(presenter.currentAmountBalance
-                                ?: 0, presenter.data?.watchMarket?.market?.amountAssetDecimals
+                        val isValid = it.toBigDecimal() <= MoneyUtil.getScaledText((presenter.currentAmountBalance
+                                ?: 0) - Constants.WAVES_DEX_FEE, presenter.data?.watchMarket?.market?.amountAssetDecimals
                                 ?: 0).clearBalance().toBigDecimal()
                         presenter.amountValidation = isValid
 
