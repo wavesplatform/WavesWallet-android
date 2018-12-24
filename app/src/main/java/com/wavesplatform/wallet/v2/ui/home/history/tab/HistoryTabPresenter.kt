dbd5dec1e51db4272745f2bef6bfe5098f349d4c
Ershov <aershov@wavesplatform.com>
2018-12-24 16:26:37 +0300

    Fix DROID-425 Empty asset history

--- app/src/main/java/com/wavesplatform/wallet/v2/ui/home/history/tab/HistoryTabPresenter.kt
+++ app/src/main/java/com/wavesplatform/wallet/v2/ui/home/history/tab/HistoryTabPresenter.kt
@@ -15,7 +15,6 @@ import com.wavesplatform.wallet.v2.data.model.remote.response.Transaction
 import com.wavesplatform.wallet.v2.ui.base.presenter.BasePresenter
 import com.wavesplatform.wallet.v2.ui.home.wallet.assets.details.content.AssetDetailsContentPresenter
 import com.wavesplatform.wallet.v2.util.isWavesId
-import com.wavesplatform.wallet.v2.util.notNull
 import io.reactivex.Single
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
@@ -110,25 +109,19 @@ class HistoryTabPresenter @Inject constructor() : BasePresenter<HistoryTabView>(
 
         return singleData.map {
             allItemsFromDb = if (assetBalance == null) {
-                // all history
-                it.sortedByDescending { it.timestamp }
+                it.sortedByDescending { transaction -> transaction.timestamp }
             } else {
-                // history only for detailed asset
-                allItemsFromDb.filter { transaction ->
-                    val assetId = assetBalance!!.assetId
-                    when {
-                        assetId.isWavesId() ->
-                            return@filter transaction.assetId.isNullOrEmpty()
-                        AssetDetailsContentPresenter.isAssetIdInExchange(
-                                transaction, assetId) ->
-                            return@filter true
-                        else ->
-                            return@filter transaction.assetId == assetId
-                    }
-                }
+                filterDetailed(it, assetBalance!!.assetId)
             }
-
             return@map sortAndConfigToUi(allItemsFromDb)
+        }
+    }
+
+    private fun filterDetailed(transactions: List<Transaction>, assetId: String): List<Transaction> {
+        return transactions.filter { transaction ->
+            assetId.isWavesId() && transaction.assetId.isNullOrEmpty()
+                    || AssetDetailsContentPresenter.isAssetIdInExchange(transaction, assetId)
+                    || transaction.assetId == assetId
         }
     }
 
