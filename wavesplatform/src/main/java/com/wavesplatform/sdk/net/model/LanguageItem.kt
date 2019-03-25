ef736e7cc81531bbb34f481fd9cf8e6232a3e7bb
Ershov <aershov@wavesplatform.com>
2019-03-25 13:47:45 +0300

    Prepare for subproject

--- wavesplatform/src/main/java/com/wavesplatform/sdk/net/model/LanguageItem.kt
+++
@@ -1,6 +0,0 @@
-package com.wavesplatform.sdk.net.model
-
-data class LanguageItem(
-    var language: Language,
-    var checked: Boolean
-)
--- wavesplatform/src/main/java/com/wavesplatform/sdk/net/model/response/UtilsTime.kt
+++
@@ -1,9 +0,0 @@
-package com.wavesplatform.sdk.net.model.response
-
-import com.google.gson.annotations.SerializedName
-
-data class UtilsTime(
-        @SerializedName("system")
-        var system: Long = 0,
-        @SerializedName("NTP")
-        var ntp: Long = 0)
