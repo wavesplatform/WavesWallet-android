3073c628c35940237894b1c62d9e809293cf6f5d
Eduard Zaydel <zaydel.eduard@gmail.com>
2019-04-03 18:52:36 +0300

    Load lease recipient address by alias

--- app/src/main/java/com/wavesplatform/wallet/v2/data/database/TransactionSaver.kt
+++ app/src/main/java/com/wavesplatform/wallet/v2/data/database/TransactionSaver.kt
@@ -138,19 +138,32 @@ class TransactionSaver @Inject constructor() {
                                 trans.feeAssetObject = allAssets.firstOrNull { it.id == trans.feeAssetId }
                             }
 
-                            if (trans.recipient.contains("alias")) {
-                                val aliasName = trans.recipient.substringAfterLast(":")
-                                aliasName.notNull {
-                                    subscriptions.add(apiDataManager.loadAlias(it)
-                                            .compose(RxUtil.applyObservableDefaultSchedulers())
-                                            .subscribe {
-                                                trans.recipientAddress = it.address
-                                                trans.transactionTypeId = transactionUtil.getTransactionType(trans)
-                                                trans.save()
-                                            })
+                            when {
+                                trans.recipient.contains("alias") -> {
+                                    val aliasName = trans.recipient.substringAfterLast(":")
+                                    aliasName.notNull {
+                                        subscriptions.add(apiDataManager.loadAlias(it)
+                                                .compose(RxUtil.applyObservableDefaultSchedulers())
+                                                .subscribe {
+                                                    trans.recipientAddress = it.address
+                                                    trans.transactionTypeId = transactionUtil.getTransactionType(trans)
+                                                    trans.save()
+                                                })
+                                    }
                                 }
-                            } else {
-                                trans.recipientAddress = trans.recipient
+                                trans.lease?.recipient?.contains("alias") == true -> {
+                                    val aliasName = trans.lease?.recipient?.substringAfterLast(":")
+                                    aliasName.notNull {
+                                        subscriptions.add(apiDataManager.loadAlias(it)
+                                                .compose(RxUtil.applyObservableDefaultSchedulers())
+                                                .subscribe {
+                                                    trans.lease?.recipientAddress = it.address
+                                                    trans.transactionTypeId = transactionUtil.getTransactionType(trans)
+                                                    trans.save()
+                                                })
+                                    }
+                                }
+                                else -> trans.recipientAddress = trans.recipient
                             }
 
                             trans.transfers.forEach { trans ->
