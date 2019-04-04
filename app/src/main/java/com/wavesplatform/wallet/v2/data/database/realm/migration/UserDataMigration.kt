46271019327b79c6ca738ee87529019d9f6e1a8f
Eduard Zaydel <zaydel.eduard@gmail.com>
2019-04-04 11:43:18 +0300

    Fix realm migration crashes. Move models to separate modules.

---
+++ app/src/main/java/com/wavesplatform/wallet/v2/data/database/realm/migration/UserDataMigration.kt
@@ -0,0 +1,23 @@
+/*
+ * Created by Eduard Zaydel on 3/4/2019
+ * Copyright © 2019 Waves Platform. All rights reserved.
+ */
+
+package com.wavesplatform.wallet.v2.data.database.realm.migration
+
+import io.realm.DynamicRealm
+import io.realm.RealmMigration
+
+class UserDataMigration : RealmMigration {
+
+    override fun migrate(realm: DynamicRealm, oldVersion: Long, newVersion: Long) {
+        var oldVersion = oldVersion
+        val schema = realm.schema
+
+        if (oldVersion <= 1L) {
+            // first migration here
+
+            oldVersion++
+        }
+    }
+}
