591f10e243b41a5ac88eaaffee370b9cbb7f85b7
Ershov <aershov@wavesplatform.com>
2019-04-29 15:20:21 +0300

    Rename Settings to DevOptions

--- app/src/main/java/com/wavesplatform/wallet/v2/ui/home/profile/settings/DevOptionsView.kt
+++ app/src/main/java/com/wavesplatform/wallet/v2/ui/home/profile/settings/DevOptionsView.kt
@@ -2,6 +2,6 @@ package com.wavesplatform.wallet.v2.ui.home.profile.settings
 
 import com.wavesplatform.wallet.v2.ui.base.view.BaseMvpView
 
-interface SettingsView : BaseMvpView {
+interface DevOptionsView : BaseMvpView {
 
 }
