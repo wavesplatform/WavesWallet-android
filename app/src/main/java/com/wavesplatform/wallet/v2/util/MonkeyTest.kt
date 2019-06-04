2f2d032081c4c0e9d17c885c552316eec7fafe1b
Ershov <aershov@wavesplatform.com>
2019-06-04 13:15:44 +0300

    Monkey test - some code refactoring

--- app/src/main/java/com/wavesplatform/wallet/v2/util/MonkeyTest.kt
+++ app/src/main/java/com/wavesplatform/wallet/v2/util/MonkeyTest.kt
@@ -12,15 +12,12 @@ class MonkeyTest {
 
     companion object {
 
-        @JvmStatic
         fun isTurnedOn(): Boolean {
             return !TextUtils.isEmpty(BuildConfig.MONKEY_TEST_SEED)
         }
 
-        @JvmStatic
         fun startIfNeed() {
             if (isTurnedOn()) {
-                //clearApplicationData()
                 val guid = App.getAccessManager().getLoggedInGuid()
                 if (TextUtils.isEmpty(guid)) {
                     App.getAccessManager().storeWalletData(
@@ -34,37 +31,6 @@ class MonkeyTest {
                     App.getAccessManager().setWallet(guid, "11111111")
                 }
             }
-        }
-
-        private fun clearApplicationData() {
-            val cache = App.getAppContext().cacheDir
-            val appDir = File(cache.parent)
-            if (appDir.exists()) {
-                val children = appDir.list()
-                for (s in children) {
-                    if (s != "lib") {
-                        deleteDir(File(appDir, s))
-                        Log.i("EEEEEERRRRRROOOOOOORRRR", "**************** File /data/data/APP_PACKAGE/$s DELETED *******************")
-                    }
-                }
-            }
-        }
-
-        private fun deleteDir(dir: File?): Boolean {
-            if (dir != null && dir.isDirectory) {
-                val children = dir.list()
-                var i = 0
-                while (i < children.size) {
-                    val success = deleteDir(File(dir, children[i]))
-                    if (!success) {
-                        return false
-                    }
-                    i++
-                }
-            }
-
-            assert(dir != null)
-            return dir!!.delete()
         }
     }
 }
