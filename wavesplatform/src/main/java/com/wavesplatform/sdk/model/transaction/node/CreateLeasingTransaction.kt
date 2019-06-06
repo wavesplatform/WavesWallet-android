3ae89bf3eb58fb980a68ef2ba081dad9257038ac
Ershov <aershov@wavesplatform.com>
2019-06-06 15:49:07 +0300

    SDK Correct sign Create Leasing

--- wavesplatform/src/main/java/com/wavesplatform/sdk/model/transaction/node/CreateLeasingTransaction.kt
+++ wavesplatform/src/main/java/com/wavesplatform/sdk/model/transaction/node/CreateLeasingTransaction.kt
@@ -50,35 +50,4 @@ class CreateLeasingTransaction(
             Base58.decode(recipient)
         }
     }
-
-
-    /*fun toSignBytes(recipientIsAlias: Boolean): ByteArray {
-        return try {
-            Bytes.concat(byteArrayOf(type.toByte()),
-                    byteArrayOf(Constants.VERSION.toByte()),
-                    byteArrayOf(0.toByte()),
-                    Base58.decode(senderPublicKey),
-                    resolveRecipientBytes(recipientIsAlias),
-                    Longs.toByteArray(amount),
-                    Longs.toByteArray(fee),
-                    Longs.toByteArray(timestamp))
-        } catch (e: Exception) {
-            Log.e("CreateLeasingRequest", "Couldn't create toSignBytes", e)
-            ByteArray(0)
-        }
-    }
-
-    private fun resolveRecipientBytes(recipientIsAlias: Boolean): ByteArray? {
-        return if (recipientIsAlias) {
-            Bytes.concat(byteArrayOf(Constants.VERSION.toByte()),
-                    byteArrayOf(EnvironmentManager.netCode),
-                    recipient.parseAlias().toByteArray(Charset.forName("UTF-8")).arrayWithSize())
-        } else {
-            Base58.decode(recipient)
-        }
-    }
-
-    fun sign(privateKey: ByteArray, recipientIsAlias: Boolean) {
-        proofs = mutableListOf(Base58.encode(CryptoProvider.sign(privateKey, toSignBytes(recipientIsAlias))))
-    }*/
 }
