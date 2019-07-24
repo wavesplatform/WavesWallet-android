74e07e077e60143312a667617605cc16170a1add
Eduard Zaydel <zaydel.eduard@gmail.com>
2019-07-24 16:43:38 +0300

    DROID-1171. Added interval model for configuration

---
+++ app/src/main/java/com/wavesplatform/wallet/v2/ui/widget/model/MarketWidgetUpdateInterval.kt
@@ -0,0 +1,40 @@
+/*
+ * Created by Eduard Zaydel on 24/7/2019
+ * Copyright © 2019 Waves Platform. All rights reserved.
+ */
+
+package com.wavesplatform.wallet.v2.ui.widget.model
+
+import android.content.Context
+import android.support.annotation.StringRes
+import com.wavesplatform.wallet.R
+import com.wavesplatform.wallet.v2.ui.widget.MarketWidget
+
+enum class MarketWidgetUpdateInterval(@StringRes title: Int, interval: Int) {
+    MIN_1(R.string.widget_interval_1_min, 1),
+    MIN_5(R.string.widget_interval_5_min, 5),
+    MIN_10(R.string.widget_interval_10_min, 10),
+    MANUALLY(R.string.widget_interval_manually, 0);
+
+    companion object {
+        private const val PREF_INTERVAL_KEY = "appwidget_interval_"
+
+        fun getInterval(context: Context, appWidgetId: Int): MarketWidgetUpdateInterval {
+            val prefs = context.getSharedPreferences(MarketWidget.PREFS_NAME, 0)
+            val interval = prefs.getString(PREF_INTERVAL_KEY + appWidgetId, null)
+            return values().firstOrNull { it.name == interval } ?: MIN_5
+        }
+
+        fun setInterval(context: Context, appWidgetId: Int, interval: MarketWidgetUpdateInterval) {
+            val prefs = context.getSharedPreferences(MarketWidget.PREFS_NAME, 0).edit()
+            prefs.putString(PREF_INTERVAL_KEY + appWidgetId, interval.name)
+            prefs.apply()
+        }
+
+        fun removeInterval(context: Context, appWidgetId: Int) {
+            val prefs = context.getSharedPreferences(MarketWidget.PREFS_NAME, 0).edit()
+            prefs.remove(PREF_INTERVAL_KEY + appWidgetId)
+            prefs.apply()
+        }
+    }
+}
