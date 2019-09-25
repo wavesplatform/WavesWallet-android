473092fa60dd96e5f793da41ee6ac6c869c33a68
Eduard Zaydel <zaydel.eduard@gmail.com>
2019-09-25 16:41:12 +0300

    DROID-1400. Move app info from drawer to profile tab, refactoring, redesign

--- app/src/main/java/com/wavesplatform/wallet/v2/ui/base/view/BaseDrawerActivity.kt
+++ app/src/main/java/com/wavesplatform/wallet/v2/ui/base/view/BaseDrawerActivity.kt
@@ -5,28 +5,17 @@
 
 package com.wavesplatform.wallet.v2.ui.base.view
 
-import android.content.Intent
 import android.graphics.Color
-import android.graphics.Paint
-import android.graphics.drawable.Drawable
-import android.net.Uri
 import android.os.Bundle
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
-import com.novoda.simplechromecustomtabs.SimpleChromeCustomTabs
-import com.wavesplatform.wallet.App
 import com.wavesplatform.wallet.R
-import com.wavesplatform.wallet.v2.data.Constants
 import com.wavesplatform.wallet.v2.data.analytics.AnalyticEvents
 import com.wavesplatform.wallet.v2.data.analytics.analytics
-import com.wavesplatform.wallet.v2.ui.welcome.WelcomeActivity
-import com.wavesplatform.wallet.v2.util.launchActivity
-import com.wavesplatform.wallet.v2.util.openUrlWithChromeTab
 import com.yarolegovich.slidingrootnav.SlidingRootNav
 import com.yarolegovich.slidingrootnav.SlidingRootNavBuilder
 import com.yarolegovich.slidingrootnav.callback.DragStateListener
-import kotlinx.android.synthetic.main.content_menu_left_drawer.view.*
+import kotlinx.android.synthetic.main.content_app_info.view.*
 import pers.victor.ext.*
 
 abstract class BaseDrawerActivity : BaseActivity() {
@@ -34,7 +23,6 @@ abstract class BaseDrawerActivity : BaseActivity() {
     lateinit var slidingRootNav: SlidingRootNav
     private var view: View? = null
     private var drawerIcon = findDrawable(R.drawable.ic_toolbar_menu)
-    private var counter = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -71,7 +59,7 @@ abstract class BaseDrawerActivity : BaseActivity() {
                 }
                 .withMenuOpened(false)
                 .withSavedState(savedInstanceState)
-                .withMenuLayout(R.layout.content_menu_left_drawer)
+                .withMenuLayout(R.layout.content_app_info) // TODO: Change to correct layout
                 .inject()
 
         toolbar.navigationIcon = drawerIcon
@@ -79,69 +67,6 @@ abstract class BaseDrawerActivity : BaseActivity() {
         createCloseView()
         slidingRootNav.layout.linear_drawer.scaleX = 1.5f
         slidingRootNav.layout.linear_drawer.scaleY = 1.5f
-        slidingRootNav.layout.text_site.paintFlags = slidingRootNav.layout.text_site.paintFlags or Paint.UNDERLINE_TEXT_FLAG
-        slidingRootNav.layout.text_site.click {
-            analytics.trackEvent(AnalyticEvents.WavesMenuForumEvent)
-            openUrlWithChromeTab(Constants.URL_WAVES_FORUM)
-        }
-        slidingRootNav.layout.text_whitepaper.click {
-            analytics.trackEvent(AnalyticEvents.WavesMenuWhitepaperEvent)
-            openUrlWithIntent(Constants.URL_WHITEPAPER)
-        }
-        slidingRootNav.layout.text_terms.click {
-            analytics.trackEvent(AnalyticEvents.WavesMenuTermsAndConditionsEvent)
-            openUrlWithIntent(Constants.URL_TERMS)
-        }
-        slidingRootNav.layout.text_support.click {
-            analytics.trackEvent(AnalyticEvents.WavesMenuSupportEvent)
-            openUrlWithChromeTab(Constants.SUPPORT_SITE)
-        }
-        slidingRootNav.layout.image_discord.click {
-            analytics.trackEvent(AnalyticEvents.WavesMenuDiscordEvent)
-            openDiscord(Constants.URL_DISCORD)
-        }
-        slidingRootNav.layout.image_reddit.click {
-            analytics.trackEvent(AnalyticEvents.WavesMenuRedditEvent)
-            openReddit(Constants.URL_REDDIT)
-        }
-        slidingRootNav.layout.image_github.click {
-            analytics.trackEvent(AnalyticEvents.WavesMenuGithubEvent)
-            openUrlWithChromeTab(Constants.URL_GITHUB)
-        }
-        slidingRootNav.layout.image_telegram.click {
-            analytics.trackEvent(AnalyticEvents.WavesMenuTelegramEvent)
-            openTelegram(Constants.ACC_TELEGRAM)
-        }
-        slidingRootNav.layout.image_twitter.click {
-            analytics.trackEvent(AnalyticEvents.WavesMenuTwitterEvent)
-            openTwitter(Constants.ACC_TWITTER)
-        }
-
-        becomeDeveloper()
-    }
-
-    private fun becomeDeveloper() {
-        slidingRootNav.layout.image_logo.click {
-            counter++
-            if (counter > 3) {
-                if (preferencesHelper.isDeveloper()) {
-                    Toast.makeText(this, "No need. You are already developer!", Toast.LENGTH_LONG).show()
-                } else {
-                    if (counter == 10) {
-                        counter = 0
-                        Toast.makeText(this, "You are developer!", Toast.LENGTH_LONG).show()
-                        preferencesHelper.setDeveloper(true)
-
-                        App.getAccessManager().resetWallet()
-                        App.getAccessManager().setLastLoggedInGuid("")
-                        finish()
-                        launchActivity<WelcomeActivity>(clear = true)
-                    } else {
-                        Toast.makeText(this, "You are almost developer! (${10 - counter})", Toast.LENGTH_LONG).show()
-                    }
-                }
-            }
-        }
     }
 
     private fun createCloseView() {
@@ -159,99 +84,5 @@ abstract class BaseDrawerActivity : BaseActivity() {
         slidingRootNav.layout.findViewById<ViewGroup>(R.id.root).addView(view)
 
         view?.gone()
-    }
-
-    fun openUrlWithIntent(url: String) {
-        if (isNetworkConnected()) {
-            val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
-            startActivity(browserIntent)
-        } else {
-            showNetworkError()
-        }
-    }
-
-    fun openFacebook(url: String) {
-        var uri = Uri.parse(url)
-        try {
-            val applicationInfo = packageManager.getApplicationInfo("com.facebook.katana", 0)
-            if (applicationInfo.enabled) {
-                // http://stackoverflow.com/a/24547437/1048340
-                uri = Uri.parse("fb://facewebmodal/f?href=$url")
-            }
-            startActivity(Intent(Intent.ACTION_VIEW, uri))
-        } catch (ignored: Exception) {
-            openUrlWithChromeTab(url)
-        }
-    }
-
-    fun openTwitter(url: String) {
-        var intent: Intent? = null
-        try {
-            // get the Twitter app if possible
-            this.packageManager.getPackageInfo("com.twitter.android", 0)
-            intent = Intent(Intent.ACTION_VIEW, Uri.parse("twitter://user?user_id=$url"))
-            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-            this.startActivity(intent)
-        } catch (e: Exception) {
-            // no Twitter app, revert to browser
-            openUrlWithChromeTab(Constants.URL_TWITTER)
-        }
-    }
-
-    fun openDiscord(url: String) {
-        var intent: Intent? = null
-        try {
-            this.packageManager.getPackageInfo("com.discord", 0)
-            intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
-            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-            this.startActivity(intent)
-        } catch (e: Exception) {
-            openUrlWithChromeTab(Constants.URL_DISCORD)
-        }
-    }
-
-    fun openReddit(url: String) {
-        var intent: Intent? = null
-        try {
-            this.packageManager.getPackageInfo("com.reddit.frontpage", 0)
-            intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
-            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-            this.startActivity(intent)
-        } catch (e: Exception) {
-            openUrlWithChromeTab(url)
-        }
-    }
-
-    fun openTelegram(url: String) {
-        var intent: Intent? = null
-        try {
-            this.packageManager.getPackageInfo("org.telegram.messenger", 0)
-            intent = Intent(Intent.ACTION_VIEW, Uri.parse("tg://resolve?domain=$url"))
-            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-            startActivity(intent)
-        } catch (e: Exception) {
-            try {
-                this.packageManager.getPackageInfo("org.thunderdog.challegram", 0)
-                intent = Intent(Intent.ACTION_VIEW, Uri.parse("tg://resolve?domain=$url"))
-                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-                startActivity(intent)
-            } catch (e: Exception) {
-                openUrlWithChromeTab(Constants.URL_TELEGRAM)
-            }
-        }
-    }
-
-    override fun onResume() {
-        super.onResume()
-        SimpleChromeCustomTabs.getInstance().connectTo(this)
-    }
-
-    override fun onPause() {
-        SimpleChromeCustomTabs.getInstance().disconnectFrom(this)
-        super.onPause()
-    }
-
-    fun changeDrawerMenuIcon(icon: Drawable?) {
-        drawerIcon = icon
     }
 }
